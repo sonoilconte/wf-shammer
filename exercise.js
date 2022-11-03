b58035553d5080dc977280d8269bddcb4eaa76a6
sonoilconte <sepp.r.hammer@gmail.com>
2022-11-03 14:05:58 -0400

    Fix return mistake, variable mistake, add summarizeNodesHash function

--- exercise.js
+++ exercise.js
@@ -2,20 +2,18 @@ const axios = require('axios');
 const BASE_URL = 'https://nodes-on-nodes-challenge.herokuapp.com/nodes/';
 
 const requestNodes = async (nodeIds, nodesHash) => {
-
     console.log(nodesHash);
     console.log('Number of keys', Object.keys(nodesHash).length);
 
     const nodesString = nodeIds.join(',');
 
     let data;
-
     let childNodeIds = [];
     try {
         ({ data } = await axios.get(`${BASE_URL}${nodesString}`));
     } catch (err) {
         console.log(err);
-        return nodesHash;
+        return false;
     }
     
     if (data) {
@@ -25,29 +23,49 @@ const requestNodes = async (nodeIds, nodesHash) => {
     }
 
     let newNodeFound = false;
-    nodeIds.forEach(nodeId => {
+    
+    childNodeIds.forEach(nodeId => {
         if (nodesHash[nodeId] === undefined) {
             nodesHash[nodeId] = 1;
             newNodeFound = true;
+            console.log('NEW')
         } else {
             nodesHash[nodeId] += 1;
+            console.log('OLD')
         }
     });
 
     if (newNodeFound) {
-        requestNodes(childNodeIds, nodesHash);
+        return requestNodes(childNodeIds, nodesHash);
     }
-
     return nodesHash;
+};
+
+const summarizeNodesHash = (nodesHash) => {
+    let mostCommonNodes = [];
+    let currentMaxOccurence = 0;
+    for (key in nodesHash) {
+        if (nodesHash[key] === currentMaxOccurence) { // multiple nodes could appear with same max number of occurences
+            mostCommonNodes.push(key);
+        }
+        if (nodesHash[key] > currentMaxOccurence) {
+            mostCommonNodes = [key];
+            currentMaxOccurence = nodesHash[key];
+        }
+    }
+    return [Object.keys(nodesHash).length, mostCommonNodes];
 }
 
 const summarizeGraph = async () => {
-    const nodesHash = await requestNodes(['089ef556-dfff-4ff2-9733-654645be56fe'], {});
-    return nodesHash;
+    const INITIAL_NODE_ID = '089ef556-dfff-4ff2-9733-654645be56fe';
+    const initialNodesHash = {};
+    initialNodesHash[INITIAL_NODE_ID] = 1;
+    const nodesHash = await requestNodes([INITIAL_NODE_ID], initialNodesHash);
+    const summary = summarizeNodesHash(nodesHash);
+    return summary;
 }
 
-
-(async function() {
+(async () => {
     const result = await summarizeGraph();
     console.log({ result });
 })();
