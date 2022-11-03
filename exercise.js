5727ae07480f08439bda4549ff29dd622d519ae6
sonoilconte <sepp.r.hammer@gmail.com>
2022-11-02 21:42:31 -0400

    Set up repo, jest

---
+++ exercise.js
@@ -0,0 +1,5 @@
+const func = () => {
+    return true;
+};
+
+module.exports = func;
---
+++ exercise.test.js
@@ -0,0 +1,5 @@
+const func = require('./exercise');
+
+it('Returns the expected value', () => {
+    expect(func()).toBe(true);
+});
